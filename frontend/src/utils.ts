d8d883e777675af426fc52507ed06e62d24392d4
Aleksey Polyakov <superpolikow@gmail.com>
2022-04-11 00:27:17 +0300

    fix functions returnins NaN

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -4,8 +4,12 @@ export type Event = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement> | 
 
 export function integerInBounds(event: Event, defaultValue: number, min: number, max: number): number {
   if (event === undefined) return defaultValue
+  if (event.target === undefined) return defaultValue
+  if (event.target.value === undefined) return defaultValue
+
 
   const value = parseInt(event.target.value)
+  if (isNaN(value)) return  defaultValue
   if (value < min) return min
   if (value > max) return max
   return value
@@ -13,8 +17,11 @@ export function integerInBounds(event: Event, defaultValue: number, min: number,
 
 export function floatInBounds(event: Event, defaultValue: number, min: number, max: number): number {
   if (event === undefined) return defaultValue
+  if (event.target === undefined) return defaultValue
+  if (event.target.value === undefined) return defaultValue
 
   const value = parseFloat(event.target.value)
+  if (isNaN(value)) return  defaultValue
   if (value < min) return min
   if (value > max) return max
   return value
