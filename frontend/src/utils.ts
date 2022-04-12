d94fb68e35c07181815248642e5d6159de990e14
Aleksey Polyakov <superpolikow@gmail.com>
2022-04-12 15:34:41 +0300

    make NaN equal to min

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -9,7 +9,7 @@ export function integerInBounds(event: Event, defaultValue: number, min: number,
 
 
   const value = parseInt(event.target.value)
-  if (isNaN(value)) return  defaultValue
+  if (isNaN(value)) return min
   if (value < min) return min
   if (value > max) return max
   return value
@@ -21,7 +21,7 @@ export function floatInBounds(event: Event, defaultValue: number, min: number, m
   if (event.target.value === undefined) return defaultValue
 
   const value = parseFloat(event.target.value)
-  if (isNaN(value)) return  defaultValue
+  if (isNaN(value)) return min
   if (value < min) return min
   if (value > max) return max
   return value
