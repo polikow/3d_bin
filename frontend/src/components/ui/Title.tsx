95d7e30dac07089f38122edbc74247100b4479bd
Aleksey Polyakov <superpolikow@gmail.com>
2022-04-20 18:48:11 +0300

    reduce title size

--- frontend/src/components/ui/Title.tsx
+++ frontend/src/components/ui/Title.tsx
@@ -7,7 +7,7 @@ interface TitleProps {
 
 const CustomTypography = styled(Typography)`
   border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-  padding: 10px 15px;
+  padding: 8px 15px;
   background: #3f51b5;
   border-radius: 4px 4px 0 0;
   color: white;
