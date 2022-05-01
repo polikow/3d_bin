2818ad2e718ffca9d179247e00bd8ea51c8c7b32
Aleksey Polyakov <superpolikow@gmail.com>
2022-05-01 20:42:25 +0300

    change light

--- frontend/src/components/canvas/Light.tsx
+++ frontend/src/components/canvas/Light.tsx
@@ -1,3 +1,8 @@
-import React from "react";
+import {Vector3} from "three";
+import {max} from "../../consts";
 
-export default () => <ambientLight intensity={1}/>
+export default () => <>
+  <hemisphereLight args={[0xB1E1FF, 0xaaaaaa, 0.3]}/>
+  <ambientLight intensity={1}/>
+  <directionalLight args={[0xffffff, 0.7]} position={new Vector3(max, max, max)}/>
+</>
